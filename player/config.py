77dbed96fc5213efa769ba6b54ceaa0383e0f9ed
Motonori Kamioka <moto.rx78@gmail.com>
2024-05-09 13:09:48 +0900

    add mcts_player

---
+++ player/config.py
@@ -0,0 +1,31 @@
+# デフォルトGPU ID
+DEFAULT_GPU_ID = 0
+# デフォルトバッチサイズ
+DEFAULT_BATCH_SIZE = 32
+# デフォルト投了閾値
+DEFAULT_RESIGN_THRESHOLD = 0.01
+# デフォルトPUCTの定数
+DEFAULT_C_PUCT = 1.0
+# デフォルト温度パラメータ
+DEFAULT_TEMPERATURE = 1.0
+# デフォルト持ち時間マージン(ms)
+DEFAULT_TIME_MARGIN = 1000
+# デフォルト秒読みマージン(ms)
+DEFAULT_BYOYOMI_MARGIN = 100
+# デフォルトPV表示間隔(ms)
+DEFAULT_PV_INTERVAL = 500
+# デフォルトプレイアウト数
+DEFAULT_CONST_PLAYOUT = 1000
+# 勝ちを表す定数（数値に意味はない）
+VALUE_WIN = 10000
+# 負けを表す定数（数値に意味はない）
+VALUE_LOSE = -10000
+# 引き分けを表す定数（数値に意味はない）
+VALUE_DRAW = 20000
+# キューに追加されたときの戻り値（数値に意味はない）
+QUEUING = -1
+# 探索を破棄するときの戻り値（数値に意味はない）
+DISCARDED = -2
+# Virtual Loss
+VIRTUAL_LOSS = 1
+
